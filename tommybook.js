d35c2604f927686b40534ea66333bce5d973637b
tom <tommy.surjadi@gmail.com>
2019-02-19 08:49:35 -0800

    created tom website

---
+++ tommybook.js
@@ -0,0 +1,8 @@
+function functionOne() {
+
+    let name = prompt('enter your name');
+    let interest = prompt('real estate or coffee');
+
+alert('Welcome '+ name + '! You are in the right website now.');
+alert('Please remember to provide your email address for news and update.');
+}
