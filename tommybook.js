a2574f4521f77ee01fa20e1a13afee8e5dfd1a6d
tom <tommy.surjadi@gmail.com>
2019-02-27 15:23:06 -0800

    Added content, css and media query and link

--- tommybook.js
+++ tommybook.js
@@ -1,8 +1,4 @@
-function functionOne() {
+alert('Welcome to Tommybook! ');
+// alert('not facebook, but tommybook..')
+// alert('Please remember to provide your email address for news and update.');
 
-    let name = prompt('enter your name');
-    let interest = prompt('real estate or coffee');
-
-alert('Welcome '+ name + '! You are in the right website now.');
-alert('Please remember to provide your email address for news and update.');
-}
