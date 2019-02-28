76382123a744ce75afae8cdceed54ca4d03be86a
tom <tommy.surjadi@gmail.com>
2019-02-27 19:26:20 -0800

    Added content

--- tommybook.js
+++ tommybook.js
@@ -1,4 +1,4 @@
-alert('Welcome to Tommybook! ');
+// alert('Welcome to Tommybook! ');
 // alert('not facebook, but tommybook..')
 // alert('Please remember to provide your email address for news and update.');
 
