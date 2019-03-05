19cde0691d3c8e61d6ce082bfa4637c6f068856d
tom <tommy.surjadi@gmail.com>
2019-03-05 07:47:06 -0800

    Added home container and tiles

--- tommybook.js
+++ tommybook.js
@@ -2,3 +2,9 @@
 // alert('not facebook, but tommybook..')
 // alert('Please remember to provide your email address for news and update.');
 
+let 
+function showHomeInfo(home) {
+    console.log(home);
+    alert(house.name);
+}
+
