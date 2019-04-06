8a1767a5c53041420dc19a416ac5051ee80ed646
tom <tommy.surjadi@gmail.com>
2019-04-06 15:34:04 -0700

    Added jquery slideToggle hide and expand

--- tommybook.js
+++ tommybook.js
@@ -1,10 +1,25 @@
 // alert('Welcome to Tommybook! ');
-// alert('not facebook, but tommybook..')
 // alert('Please remember to provide your email address for news and update.');
 
-let 
-function showHomeInfo(home) {
-    console.log(home);
-    alert(house.name);
-}
+// let home;
+// function showHomeInfo(home) {
+//     console.log(home);
+//     alert(house.name);
+// }
 
+jQuery(document).ready(function() {
+
+// initially hide the answers in the FAQ section
+
+jQuery(".answer").hide();
+
+//handling the FAQ questions click
+
+jQuery(".faq h4").click(function() {
+
+    jQuery(this).next(".answer").slideToggle();
+});
+
+jQuery(".faq h4").addClass("faq-questions");
+
+});
